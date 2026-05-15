ecaa500337169272f0a2d4d44027ef901a2cc1e2
Preetish <preetishramola111@gmail.com>
2026-05-15 14:16:12 +0530

    chatResponse : for meta data

--- src/main/java/com/example/springAI/OpenAIController.java
+++ src/main/java/com/example/springAI/OpenAIController.java
@@ -1,8 +1,9 @@
 package com.example.springAI;
 
 import org.springframework.ai.chat.client.ChatClient;
-import org.springframework.ai.chat.client.ResponseEntity;
+import org.springframework.ai.chat.model.ChatResponse;
 import org.springframework.ai.openai.OpenAiChatModel;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,10 +19,22 @@ public class OpenAIController {
 
     @GetMapping("/api/{message}")
     public ResponseEntity<String> getAnswer(@PathVariable String message){
-        String response=chatClient
+//        String response=chatClient
+//                .prompt(message)
+//                .call()
+//                .content();
+//        return ResponseEntity.ok(response);
+        ChatResponse chatResponse=chatClient
                 .prompt(message)
                 .call()
-                .content();
+                .chatResponse();//for meta data
+
+        System.out.println(chatResponse.getMetadata().getModel());
+
+        String response=chatResponse
+                .getResult()
+                .getOutput()
+                .getText();
         return ResponseEntity.ok(response);
     }
 }
